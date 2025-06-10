0172f70826d80e8ad8836eb8071bb758990facd7
mark <mmackenzie.uk@gmail.com>
2025-06-10 20:58:10 +1000

    test with charm

--- nordic-frontend/app/config/index.ts
+++ nordic-frontend/app/config/index.ts
@@ -8,7 +8,7 @@ const HOST = process.env.HOST;
 
 export const PORT = 5000;
 
-export const APP_TEMPLATE = "WWURM"; 
+export const APP_TEMPLATE = "CHARM"; 
 
 export const API_ENDPOINT = `http://${HOST}:${PORT}`;
 
